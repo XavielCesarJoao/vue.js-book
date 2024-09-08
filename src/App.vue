5a98adf3a829be255fea2abdb9b07caaaf771a00
XavielCesarJoao <134645093+XavielCesarJoao@users.noreply.github.com>
2024-09-08 12:41:16 +0100

    Neste capitulo abordamos sobre Watch

--- src/App.vue
+++ src/App.vue
@@ -1,107 +1,84 @@
 <template>
   <div>
-    <h1>
-      {{ fullName }}
-    </h1>
-    <br>
-    <br>
-
-    <h1>Todos Em Aberto</h1>
-    <div
-        v-for="todo in anCompletedTodos"
-        v-bind:key="todo.id"
+    <input class="border border-green-700"
+           type="text"
+           v-model="name"
     >
-      <div>
-        {{ todo.title }}
-
-      </div>
-    </div>
     <br>
-
-    <h1>Todos Completadas</h1>
-    <div v-for="todo in completedTodos"
-         v-bind:key="todo.id"
-    >
-      <div>
-          {{ todo.title }}
-      </div>
-      <br>
-    </div>
-
-    <h1>Todos</h1>
-    <div
-        v-for="todo in todos"
-        :key="todo.id"
-    >
-      <input
-          type="checkbox"
-          v-model="todo.completed"
+    {{ name }}
+    <br><br><br>
+    <div>
+      <label>Faça a sua questão</label>
+      <input class="border border-green-700"
+             type="text"
+             v-model="question" :disabled="loding"
       >
-      {{todo.title}}
+      <br>
+      {{ answer }}
     </div>
   </div>
-
 </template>
 
 <script>
 export default {
   name: 'App',
   components: {
-
     // TheHeader
   },
   data() {
     return {
-      user: {
-        first_name: 'Xaviel',
-        last_name: 'João',
-        age: 21,
-      },
 
-      email: '',
-      items: [
-        {id: 1, label: '10 party hats', purchased: true, highPriority: false},
-        {id: 2, label: '2 board games', purchased: true, highPriority: false},
-        {id: 3, label: '20 cups', purchased: false, highPriority: true},
-      ],
-      todos: [
-        {
-          "userId": 1,
-          "id": 1,
-          "title": "delectus aut autem",
-          "completed": false
-        },
-        {
-          "userId": 1,
-          "id": 2,
-          "title": "quis ut nam facilis et officia qui",
-          "completed": false
-        },
-        {
-          "userId": 1,
-          "id": 3,
-          "title": "fugiat veniam minus",
-          "completed": false
-        },
-        {
-          "userId": 1,
-          "id": 4,
-          "title": "et porro tempora",
-          "completed": true
-        },
-        {
-          "userId": 1,
-          "id": 5,
-          "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
-          "completed": false
+      question: '',
+      answer: 'Questions usually contain a question mark. ;-)',
+      loding: false,
+
+      name: 'Ana Maria',
+      user: {
+        first_name: '',
+        last_name: '',
+        address: {
+          province: 'Benguela',
+          street: 'Viana',
         }
-      ],
+      }
     }
   },
 
+  watch: {
+    name() {
+      this.saveUser();
+    },
+    user: {
+      handler() {
+        console.log('Legal pra caramba');
+      },
+      deep: true,
+    },
+    question(newQuestion, oldQuestion) {
+      if (newQuestion.includes('?')) {
+        this.getAnswer();
+        console.log(oldQuestion);
+      }
+    }
+  },
   methods: {
-    onClick() {
-      console.log('Porraz, vue js é massa')
+    saveUser() {
+      console.log('Solicitando Ajax');
+      setTimeout(function () {
+        console.log('Sucesso', this.name);
+      }, 7000)
+    },
+    async getAnswer() {
+      this.loding = true;
+      this.answer = 'Thinking . . . ';
+      try {
+        const res = await fetch('https://yesno.wtf/api')
+        this.answer = (await res.json()).answer
+      } catch (error) {
+        this.answer = 'Error! Could not reach the APL. ' + error
+      } finally {
+        this.loding = false;
+      }
     }
   },
 
@@ -109,13 +86,41 @@ export default {
     fullName() {
       return `${this.user.first_name} ${this.user.last_name}`
     },
-    anCompletedTodos() {
-      return this.todos.filter(todo => !todo.completed);
-    },
-    completedTodos() {
-      return this.todos.filter(todo => todo.completed)
-    }
-  }
+  },
+  // HOOKS
+  beforeCreate() {
+    console.log('beforeCreate', this.name)
+    console.log('DOM', this.$el)
+  },
+  created() {
+    console.log('created', this.name)
+    console.log('DOM', this.$el)
+  },
+
+  beforeMount() {
+    console.log('beforeMount', this.name)
+    console.log('DOM', this.$el)
+
+  },
+  mounted() {
+    console.log('mounted', this.name)
+    console.log('DOM', this.$el)
+  },
+
+  beforeUnmount() {
+    console.log('beforeUnmount')
+    console.log('DOM', this.$el)
+
+  },
+  unmounted() {
+    console.log('unmounted')
+    console.log('DOM', this.$el)
+  },
+  beforeUpdate() {
+  },
+
+  updated() {
+  },
 }
 </script>
 
